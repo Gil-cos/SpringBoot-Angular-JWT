195ff9e56cecd1935bdf8d8c6a60f8215d98a6e7
Gilberto Costa Junior <gil-cos@hotmail.com>
2019-04-17 14:05:47 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Código completo do curso **Angular 5, JWT, Spring Boot,REST,Security,Data e MongoDB**.
+Curso: https://www.udemy.com/angular-5-jwt-spring-rest
---
+++ SpringBoot-Angular-JWT-MySQL/Help-Desk-UI/src/app/app.routes.ts
@@ -0,0 +1,66 @@
+import { ModuleWithProviders } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './components/security/auth.guard';
+import { LoginComponent } from "./components/security/login/login.component";
+import { SummaryComponent } from './components/summary/summary.component';
+import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.component';
+import { TicketListComponent } from './components/ticket-list/ticket-list.component';
+import { TicketNewComponent } from './components/ticket-new/ticket-new.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserNewComponent } from './components/user-new/user-new.component';
+
+export const ROUTES: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'user-new',
+    component: UserNewComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'user-new/:id',
+    component: UserNewComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'user-list',
+    component: UserListComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'ticket-new',
+    component: TicketNewComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'ticket-new/:id',
+    component: TicketNewComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'ticket-list',
+    component: TicketListComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'ticket-detail/:id', 
+    component: TicketDetailComponent, 
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'summary', 
+    component: SummaryComponent, 
+    canActivate: [AuthGuard]
+  }
+]
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
+
